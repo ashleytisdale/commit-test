787e7300c5605fe81c013734c5084ff625b36375
mynuni <elelaka0000@gmail.com>
2024-04-22 21:38:34 +0900

    feat: test1

--- src/main/java/com/practice/Main.java
+++ src/main/java/com/practice/Main.java
@@ -2,6 +2,6 @@ package com.practice;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("commit test");
+        System.out.println("commit test2");
     }
 }
